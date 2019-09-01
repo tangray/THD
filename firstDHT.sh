3c872cd5cfa8e325c30a449be94a007e34271f24
tangray <2463873786@qq.com>
2019-09-01 19:16:55 +0800

    启动脚本

---
+++ firstDHT.sh
@@ -0,0 +1,20 @@
+java -cp bin/ NodeDHT 10.203.12.76 2019 2033 12
+
+
+#TO start the client
+#  java -cp bin/ -Djava.security.policy=src/java/policyfile.txt ClientChord localhost 1900
+
+#Syntax to start the NodeDHT:
+#Syntax one - NodeDHT-First [LocalPortnumber] [numNodes]  
+#Syntax two - NodeDHT-other [Known-HostIP]  [Known-HostPortnumber] [LocalPortnumber] [numNodes]
+# - LocalPortNumber is the number that will receive connections in the Node
+# - Known-HostName  is the hostIP of one DHTNode already in the net.
+# - Known-HostPortnumber is the port which the Known-Host listening waiting for connections.
+# - numNodes is the number of the DHTNode you designed 
+
+#NodeDHT first
+# java -cp bin/ NodeDHT 55511 numNodes
+
+#NodeDHT first
+# java -cp bin/ NodeDHT IP 55511 55512 numNodes
+
